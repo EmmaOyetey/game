f56e8b99f796f37622035c99b003dd892f92f4ef
Emma Oyetey <emmaflookis@hotmail.com>
2024-03-20 13:39:53 +0000

    adjustments to the newGame Button when game over

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,7 @@ const submitGuessButton =
   document.querySelector<HTMLButtonElement>(".submitGuessButton");
 const audioStopButton =
   document.querySelector<HTMLButtonElement>(".audioPauseButton");
-const restartButton =
-  document.querySelector<HTMLButtonElement>(".restartButton");
+
 const guessInput = document.querySelector<HTMLInputElement>("#guess");
 const imageDisplay =
   document.querySelector<HTMLInputElement>(".image-container");  
@@ -27,14 +26,15 @@ const guessInputDisplay = document.querySelector<HTMLDivElement>(
 const gameAim = document.querySelector<HTMLElement>(".gameAimIntro");
 const winner = document.querySelector<HTMLElement>(".gameOutcomeWinner");
 const loser = document.querySelector<HTMLElement>(".gameOutcomeLoser");
+const gameOverButton =
+  document.querySelector<HTMLButtonElement>(".restartButton");
 
 if (
   !startNewGameButton ||
   !playAudioButton ||
   !audioStopButton ||
   !controlButtonsDisplay ||
-  !restartButton
-
+  !gameOverButton
 ) {
   throw new Error("issue with buttons");
 }
@@ -56,6 +56,14 @@ let thisAttempt: number = 0;
 const audioElement = document.createElement("audio");
 audioElement.className = "audio";
 
+const handleNewGame = (): void => {
+  window.location.reload();
+  }
+
+const stopAudio = (): void => {
+  audioElement.pause(); 
+  audioElement.src = ""; };
+
 const startNewGame = (): void => {
   thisAttempt = 0;
   const randomID: number = Math.floor(Math.random() * songArray.length);
@@ -64,127 +72,134 @@ const startNewGame = (): void => {
   inGameAudioAndFeedbackContainer.style.display = "flex";
   feedbackDisplay.textContent = "Hit play to get started"; 
   startNewGameButton.style.display = "none";
-};
 
-const handlethisGame = (): void => {
-  if (thisSong) {
-    audioElement.src = thisSong.audio[thisAttempt];
-    audioElement.play();
-    console.log("i'm playing a song!" + thisSong.artist);
-    } else {
-      throw new Error("no song selected");;
-    }
-  guessInputDisplay.style.display = "flex";
-  controlButtonsDisplay.style.display = "flex";
-  feedbackDisplay.innerHTML = `<div>
-      <p>Hear again?</p>
-      <p>Hit Play</p>
-      </div>`;
-  feedbackDisplay.style.fontSize = "1em";
-};
 
-const handleThisGuess = (): void => {
-  if (thisSong) {
-    const correctAnswer = thisSong.artist;
-    const thisGuess = guessInput.value.trim().toLowerCase();
-    thisAttempt++;
-
-    if (thisGuess !== correctAnswer && thisAttempt < 4) {
-
-      audioElement.src = "./src/audio/soundEffects/fail.mp3";
-      audioElement.play();
-      audioElement.onended = () => {
-        if (thisSong && thisSong.audio.length < 4) {
-          audioElement.src = "./src/audio/soundEffects/whoosh.mp3"; 
-          audioElement.play();
+    const handlethisGame = (): void => {
+      if (thisSong) {
+        audioElement.src = thisSong.audio[thisAttempt];
+        audioElement.play();
+        console.log("i'm playing a song!" + thisSong.audio[thisAttempt]);
+        } else {
+          throw new Error("no song selected");;
         }
-      };
-      feedbackDisplay.textContent = "Nope! Hit play to hear some more";
-      controlButtonsDisplay.style.display = "flex";
+      guessInputDisplay.style.display = "flex";
+      feedbackDisplay.textContent = "Hear again? Hit Play";
       feedbackDisplay.style.fontSize = "1.2em";
-      guessInputDisplay.style.display = "none";
-      guessInput.value = "";
-      guessInput.placeholder = "Guess again!";
+    };
 
-      } else if (thisGuess === correctAnswer?.toLowerCase()) {
+    const handleThisGuess = (): void => {
+      if (thisSong) {
+        const correctAnswer = thisSong.artist;
+        const thisGuess = guessInput.value.trim().toLowerCase();
+        thisAttempt += 1;
+        console.log("this is guess" + thisAttempt)
 
-          audioElement.src = "./src/audio/soundEffects/certifiedBadass.mp3";
+        if (thisGuess !== correctAnswer && thisAttempt < 4) {
+
+          audioElement.src = "./src/audio/soundEffects/fail.mp3";
           audioElement.play();
           audioElement.onended = () => {
-            if (thisSong && thisSong.audio.length > 4) {
-              audioElement.src = thisSong.audio[4]; 
-              audioElement.play();
+            if (thisSong && thisSong.audio.length < 4) {
+              audioElement.src = thisSong.audio[thisAttempt]; 
+              audioElement.play(); 
             }
           };
-          imageDisplay.classList.remove("image-container");
-          imageDisplay.classList.add("image-container-winner");
-          winner.style.display = "flex";
-          imageDisplay.innerHTML = `<div style="font-size: 1.5em">
-                  <p style="margin: 0;">You were listening to ${thisSong.songTitle}</p>
-                  <p style="margin: 0;">by ${thisSong.artist}</p>
-                  <p style="margin: 0;">from the Album ${thisSong.albumName}</P>
-                  <p style="margin: 0;">released in ${thisSong.releaseDate}</p>
-                </div>`;
-          gameAim.style.display = "none";
-          playAudioButton.style.display = "none";
+          feedbackDisplay.textContent = "Nope! Hit play to hear some more";
+          controlButtonsDisplay.style.display = "flex";
+          feedbackDisplay.style.fontSize = "1.2em";
           guessInputDisplay.style.display = "none";
-          inGameAudioAndFeedbackContainer.style.display = "none";
-          thisAttempt = 0;
+          guessInput.value = "";
+          guessInput.placeholder = "Guess again!";
 
+          } else if (thisGuess === correctAnswer?.toLowerCase()) {
 
-        } else if (thisGuess !== correctAnswer && (thisAttempt = 4)) {
-            audioElement.src = "./src/audio/soundEffects/youSuck.mp3";
-            audioElement.play();
-            audioElement.onended = () => {
-              if (thisSong && thisSong.audio.length > 4) {
-                audioElement.src = thisSong.audio[4];
-                audioElement.play();
-              }
-            };
-            loser.style.display = "flex";
-            imageDisplay.innerHTML = `<div style="font-size: 1.5em">
-                    <p style="margin: 0;">You were listening to ${thisSong.songTitle}</p>
-                    <p style="margin: 0;">by ${thisSong.artist}</p>
-                    <p style="margin: 0;">from the Album ${thisSong.albumName}</P>
-                    <p style="margin: 0;">released in ${thisSong.releaseDate}</p>
+              audioElement.src = "./src/audio/soundEffects/certifiedBadass.mp3";
+              audioElement.play();
+              audioElement.onended = () => {
+                if (thisSong && thisSong.audio.length > 4) {
+                  audioElement.src = thisSong.audio[4]; 
+                  audioElement.play();
+                }
+              };
+              imageDisplay.classList.remove("image-container");
+              imageDisplay.classList.add("image-container-winner");
+              winner.style.display = "flex";
+              imageDisplay.innerHTML = `<div style="font-size: 1.5em">
+                      <p style="margin: 0;">You were listening to ${thisSong.songTitle}</p>
+                      <p style="margin: 0;">by ${thisSong.artist}</p>
+                      <p style="margin: 0;">from the Album ${thisSong.albumName}</P>
+                      <p style="margin: 0;">released in ${thisSong.releaseDate}</p>
                     </div>`;
-            imageDisplay.classList.remove("image-container");
-            imageDisplay.classList.add("image-container-winner");
-            gameAim.style.display = "none";
-            inGameAudioAndFeedbackContainer.style.display = "none";
-            guessInputDisplay.style.display = "none";
-            thisAttempt = 0;
-            } else {
-              throw new Error ("problem handling multiple guesses");
-            }
-  } else {
-    throw new Error ("no song for playback");
-     }
-};
+              gameAim.style.display = "none";
+              guessInputDisplay.style.display = "none";
+              inGameAudioAndFeedbackContainer.style.display = "none";
+              thisAttempt = 0;
 
-const handleReset = (): void => {
-  startNewGame();
-  gameAim.style.display = "flex";
-  imageDisplay.classList.add("image-container");
-  startNewGameButton.style.display = "flex";
-  //imageDisplay.classList.remove("image-container-winner");
-  //imageDisplay.classList.remove("image-container-loser");
-  imageDisplay.innerHTML = ``;
-  guessInputDisplay.style.display = "none";
-  inGameAudioAndFeedbackContainer.style.display = "none";
-  winner.style.display = "none";
-  loser.style.display = "none";
-  stopAudio ();
-  thisAttempt = 0;
-};
 
-const stopAudio = (): void => {
-  audioElement.pause();
-  audioElement.currentTime = 0;
-};
+            } else if (thisGuess !== correctAnswer && (thisAttempt = 4)) {
+                audioElement.src = "./src/audio/soundEffects/youSuck.mp3";
+                audioElement.play();
+                audioElement.onended = () => {
+                  if (thisSong && thisSong.audio.length > 4) {
+                    audioElement.src = thisSong.audio[4];
+                    audioElement.play();
+                  }
+                };
+                loser.style.display = "flex";
+                imageDisplay.innerHTML = `<div style="font-size: 1.5em">
+                        <p style="margin: 0;">You were listening to ${thisSong.songTitle}</p>
+                        <p style="margin: 0;">by ${thisSong.artist}</p>
+                        <p style="margin: 0;">from the Album ${thisSong.albumName}</P>
+                        <p style="margin: 0;">released in ${thisSong.releaseDate}</p>
+                        </div>`;
+                imageDisplay.classList.remove("image-container");
+                imageDisplay.classList.add("image-container-winner");
+                gameAim.style.display = "none";
+                inGameAudioAndFeedbackContainer.style.display = "none";
+                guessInputDisplay.style.display = "none";
+                thisAttempt = 0;
+                } else {
+                  throw new Error ("problem handling multiple guesses");
+                }
+      } else {
+        throw new Error ("no song for playback");
+        }
+    };
 
-playAudioButton.addEventListener("click", handlethisGame);
+
+
+    playAudioButton.addEventListener("click", handlethisGame);
+    submitGuessButton.addEventListener("click", handleThisGuess);
+    audioStopButton.addEventListener("click", stopAudio);
+    gameOverButton.addEventListener("click", handleNewGame);
+
+  };
+
+
+
+
+
 startNewGameButton.addEventListener("click", startNewGame);
-submitGuessButton.addEventListener("click", handleThisGuess);
-restartButton.addEventListener("click", handleReset);
-audioStopButton.addEventListener("click", stopAudio);
+
+
+//const handleReset = (): void => {
+//  stopAudio();
+ // audioElement.src = "";
+//  winner.style.display = "none";
+//  loser.style.display = "none";
+//  gameAim.style.display = "flex";
+//  imageDisplay.classList.remove("image-container-winner");
+//  imageDisplay.classList.remove("image-container-loser");
+//  imageDisplay.classList.add("image-container");
+//  imageDisplay.innerHTML = ``;
+//  startNewGameButton.style.display = "flex";
+//  guessInputDisplay.style.display = "none";
+//  inGameAudioAndFeedbackContainer.style.display = "none";
+//};
+
+
+
+
+
+
+
